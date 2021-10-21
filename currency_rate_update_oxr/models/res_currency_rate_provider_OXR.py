479cd195fef7266c33f5c3d47b934f824e7f50fc
Kevin Graveman <kevin.graveman@vanmoof.com>
2021-10-21 08:38:00 +0200

    compatibility for older supported Python versions

--- currency_rate_update_oxr/models/res_currency_rate_provider_OXR.py
+++ currency_rate_update_oxr/models/res_currency_rate_provider_OXR.py
@@ -66,7 +66,11 @@ class ResCurrencyRateProviderOXR(models.Model):
                 )
             data_date = datetime.utcfromtimestamp(data['timestamp']).date()
             if eod_rates and data_date != date:
-                raise UserError(f"Exchange rate from incorrect date received. Got {data_date}, expected {date}")
+                raise UserError(
+                    "Exchange rate from incorrect date received. Got {data_date}, expected {date}".format(
+                        data_date=data_date, date=date
+                    )
+                )
             date_content = content[(date + timedelta(days=1) if eod_rates else date).isoformat()]
             if 'rates' in data:
                 for currency, rate in data['rates'].items():
